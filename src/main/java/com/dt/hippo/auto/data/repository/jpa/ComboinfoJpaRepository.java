7aa7151182b838fec775dc9f643246452eda9ffb
王栋 <wangdong@wangdongdeMacBook-Pro.local>
2015-05-14 21:42:48 +0800

    fixed the errors

--- src/main/java/com/dt/hippo/auto/data/repository/jpa/ComboinfoJpaRepository.java
+++ src/main/java/com/dt/hippo/auto/data/repository/jpa/ComboinfoJpaRepository.java
@@ -2,11 +2,10 @@ package com.dt.hippo.auto.data.repository.jpa;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import com.dt.hippo.auto.model.jpa.ComboinfoEntity;
-import com.dt.hippo.auto.model.jpa.ComboinfoEntityKey;
 
 /**
  * Repository : Comboinfo.
  */
-public interface ComboinfoJpaRepository extends PagingAndSortingRepository<ComboinfoEntity, ComboinfoEntityKey> {
+public interface ComboinfoJpaRepository extends PagingAndSortingRepository<ComboinfoEntity, Long> {
 
 }
